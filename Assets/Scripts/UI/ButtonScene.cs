f1846aac25a4acbf6bf5cfcf0f5a69f750e6af04
Chaku <cristiangalindo13@gmail.com>
2023-11-14 02:16:47 +0100

    PlayButton effect + script

--- Assets/Scripts/UI/ButtonScene.cs
+++ Assets/Scripts/UI/ButtonScene.cs
@@ -1,28 +1,54 @@
-using Microsoft.Unity.VisualStudio.Editor;
 using UnityEngine;
 using UnityEngine.UI;
-using UnityEngine.UIElements;
 
 public class ButtonScene : MonoBehaviour
 {
+    [SerializeField] private GameObject m_MainMenu;
     [SerializeField] private GameObject m_Submenu;
-    [SerializeField] private GameObject[] m_Buttons;
+    [SerializeField] private Image[] m_ButtonsBackround;
+
+    private bool b_StartEffect = false;
+    private float f_Fade = 2;
 
     private void Start() 
     {
         m_Submenu.SetActive(false);
     }
 
+    private void Update() 
+    {
+        if(b_StartEffect == true)
+        {
+            f_Fade -= Time.deltaTime;
+
+            //Dissolve Effect on button array
+            foreach(Image obj in m_ButtonsBackround)
+            {
+                obj.material.SetFloat("_Fade",f_Fade);
+            }
+
+            if(f_Fade <= 0)
+            {
+                //Restart the values
+                b_StartEffect = false;
+                f_Fade = 2;
+
+                m_Submenu.SetActive(true);
+
+                foreach(Image obj in m_ButtonsBackround)
+                {
+                    obj.material.SetFloat("_Fade",1);
+                }
+                
+                //Last (turn off the script)
+                m_MainMenu.SetActive(false);
+            }
+        }
+    }
+
     public void PressButton()
     {
-        //Effecto desaparecer sprites
-
-        foreach(GameObject obj in m_Buttons)
-        {
-            //Image img = obj.GetComponent<Image>();
-        }
-
-        m_Submenu.SetActive(true);
-
+        // Open pass Update
+        b_StartEffect = true;
     }
 }
